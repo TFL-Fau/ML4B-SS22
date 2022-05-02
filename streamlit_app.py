8419ec7518d13879a73fe2445da3311f085a3013
TFL-Fau <t.fleming@gmx.de>
2022-05-02 20:03:44 +0200

    nealry finished

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,5 @@
 import streamlit as st
-import glob
 import jsonlines
-import pandas as pd
-from pprint import pprint
 import random
 
 #Group Introduction Code
@@ -18,7 +15,7 @@ with col1:
 
 with col2:
     
-    st.image("logoOfficial.png", width=100)
+    st.image("twitter.png", width=100)
 
 with col3:
     st.write(' ')
@@ -33,15 +30,15 @@ with st.expander("Click here for Team presentation", expanded=False):
     row1_1, row1_2, row1_3 = st.columns((1, 1, 1))
     with row1_1:
         st.subheader("1. Teammember")
-        st.image("Philip_Maron.jpg", width=215)
+        st.image("Philip.jpg", width=215)
         st.markdown("<p style='text-align: center; color: white;'>Philip Maron <br> <br>  Eighth semester Business Informatics </p>",unsafe_allow_html=True)
     with row1_2:
         st.subheader("2. Teammember")
-        st.image("Tobias_Fleming.jpg", width=200)
+        st.image("Tobias.jpg", width=200)
         st.markdown("<p style='text-align: center; color: white;'>Tobias Fleming <br> <br> Sixth semester Business Informatics </p>",unsafe_allow_html=True)
     with row1_3:
         st.subheader("3. Teammember")
-        st.image("Thies_Freudenthal_Maske_neu.png", width=219)
+        st.image("Thies.png", width=219)
         st.markdown("<p front-size= 30px style='text-align: center; color: white;'>Thies Freudenthal<br> <br>Sixth semester Business Informatics </p>",unsafe_allow_html=True)
 
 st.markdown("<h4 style='text-align: center; color: white;'>Projectdiscription</h4>",unsafe_allow_html=True)
@@ -50,7 +47,7 @@ with st.expander("Click here for Projectdiscription", expanded=False):
     
     st.markdown("<h5 style='text-align: center; color: white;'>Hello we are Philip, Tobias and Thies and we want to create a Twitter Sentiment Analyzer for comments of Members of the German Parliament. For this we want to use Machine Learning to process the dataset provided to us and present the results.</h5>",unsafe_allow_html=True)
     
-#First Analysis of data from Olaf Scholz to get into the data
+#First Analysis of data from Olaf Scholz to get into the data and generating random Olar Scholz comment
 
 result=st.button("Generate random Olaf Scholz comment", disabled=False)
 #st.write(result)
