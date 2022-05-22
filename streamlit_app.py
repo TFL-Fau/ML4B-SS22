65a576252d30e458b822eeec5036321be2413c69
TFL-Fau <t.fleming@gmx.de>
2022-05-22 15:29:06 +0200

    Finished presentation on streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import jsonlines
 import random
+import pandas as pd
+from PIL import Image
 
 #Group Introduction Code
 
@@ -74,3 +76,133 @@ if result:
 
     st.write(dates[randomnumber])
     st.write(texts[randomnumber])  
+with st.expander("Analyzing given Dataset", expanded=False):
+    # Analyzing given dataset
+
+    st.markdown("<h4 style='text-align: center; color: white;'>Analysing Dataset for our Dataframe</h4>",unsafe_allow_html=True)
+    st.write(' ')
+    st.write('- Analyzing the given Dataset')
+    st.write('- Looking for useful parameters to create a dataset')
+    st.write(' ')
+    showana=st.button("Show Dataset Analysis", disabled=False)
+    if showana:
+        with jsonlines.open(file) as j:
+            for line in j:
+                 st.write('Typ: ')
+                 st.write(type(line))
+                 st.write('Keys des Datensatzes: ')
+                 st.write(line.keys())
+                 st.write('Keys von response: ')
+                 st.write(line['response'].keys())
+                 st.write('Keys von data in response: ')
+                 st.write(line['response']['data'][0].keys())
+                 break
+    st.write('Parameters filtered for new dataframe: ')
+    st.write('- target: To store the outcome of our analysis')
+    st.write('- tweetid: Given from response, data and id')
+    st.write('- user: Given from response, data and author_id')
+    st.write('- date: Given from response, data and created at')
+    st.write('- time: Given from response, data and created at')
+    st.write('- text: Given from response, data and text')
+    
+with st.expander("Show our dataframe", expanded=False):
+    # Creating dataframe
+
+    st.markdown("<h4 style='text-align: center; color: white;'>Our Dataframe</h4>",unsafe_allow_html=True)
+    st.write(' ')
+    showdf=st.button("Show information about our Dataframe", disabled=False)
+    if showdf:
+        olafScholzJsonLines = jsonlines.open("OlafScholz.jl")
+        df = pd.DataFrame(columns = ["target", "tweetid", "date", "time", "user", "text"])
+  
+        iterator = 0
+
+        for line in olafScholzJsonLines:
+            keyResponse = line["response"]
+            data = keyResponse["data"]
+            newDataRow = [None, None, None, None, None, None]
+            userName = line["account_name"]
+            newDataRow[4] = userName
+            keyResponse = line["response"]
+            data = keyResponse["data"]
+
+
+            for tweet in data:
+             
+                tweetTarget = None
+                tweetID = tweet["id"]
+                authorID = tweet["author_id"]
+                tweetDate = tweet["created_at"][0:10]
+                tweetTime = tweet["created_at"][11:19]
+                tweetText = tweet["text"]
+                newDataRow[0] = tweetTarget
+                newDataRow[1] = tweetID
+                newDataRow[2] = tweetDate
+                newDataRow[3] = tweetTime
+                #
+                newDataRow[4] = authorID
+                newDataRow[5] = tweetText
+                
+                df.loc[len(df)] = newDataRow
+                
+            iterator += 1
+        st.write('Anzahl an Zeilen und Spalten')
+        st.write(df.shape)
+        st.write(df.head(20))
+
+with st.expander("Analyze data from Dataframe", expanded=False):
+    # Creating dataframe
+
+    st.markdown("<h4 style='text-align: center; color: white;'>Analyze data from Dataframe</h4>",unsafe_allow_html=True)
+    st.write(' ')
+    showa=st.button("Show analyze", disabled=False)
+    if showa:
+          
+        olafScholzJsonLines = jsonlines.open("OlafScholz.jl")
+        df = pd.DataFrame(columns = ["target", "tweetid", "date", "time", "user", "text"])
+  
+        iterator = 0
+
+        for line in olafScholzJsonLines:
+            keyResponse = line["response"]
+            data = keyResponse["data"]
+            newDataRow = [None, None, None, None, None, None]
+            userName = line["account_name"]
+            newDataRow[4] = userName
+            keyResponse = line["response"]
+            data = keyResponse["data"]
+
+
+            for tweet in data:
+             
+                tweetTarget = None
+                tweetID = tweet["id"]
+                authorID = tweet["author_id"]
+                tweetDate = tweet["created_at"][0:10]
+                tweetTime = tweet["created_at"][11:19]
+                tweetText = tweet["text"]
+                newDataRow[0] = tweetTarget
+                newDataRow[1] = tweetID
+                newDataRow[2] = tweetDate
+                newDataRow[3] = tweetTime
+                #
+                newDataRow[4] = authorID
+                newDataRow[5] = tweetText
+                
+                df.loc[len(df)] = newDataRow
+                
+            iterator += 1
+        st.write('First date:')
+        st.write(df['date'].min())
+        st.write('Last date:')
+        st.write(df['date'].max())
+
+        image = Image.open('dataframepic.png')
+
+        st.image(image, caption='Number of tweets per year')
+    
+    
+    
+    
+    
+            
