6ff043c60b283393ca948896d7a5f7aaf24083f7
TFL-Fau <t.fleming@gmx.de>
2022-06-15 22:07:30 +0200

    Streamlit multipage

--- my_app/streamlit_app.py
+++ my_app/streamlit_app.py
@@ -1,17 +1,3 @@
 # Contents of ~/my_app/main_page.py
 import streamlit as st
 
-st.markdown("# Main page 🎈")
-st.sidebar.markdown("# Main page 🎈")
-
-# Contents of ~/my_app/pages/page_2.py
-import streamlit as st
-
-st.markdown("# Page 2 ❄️")
-st.sidebar.markdown("# Page 2 ❄️")
-
-# Contents of ~/my_app/pages/page_3.py
-import streamlit as st
-
-st.markdown("# Page 3 🎉")
-st.sidebar.markdown("# Page 3 🎉")
