093f609089926b24d5da80e7eb78d393a1dfcb88
TFL-Fau <t.fleming@gmx.de>
2022-06-19 19:25:08 +0200

    Tried with Pickle

--- my_app/pages/page_4.py
+++ my_app/pages/page_4.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pandas as pd
 import joblib
-import pickle
-import sklearn
+#import pickle
+
 
 st.set_page_config(page_icon="🕊️", page_title="German Twitter Analysis")
 
