a24bf2e614ed7b80dce1032e16f6d9d0823457b9
NemesisEuw <86473924+NemesisEuw@users.noreply.github.com>
2022-08-20 13:00:00 +0200

    h1

--- my_app/pages/3 Algorithm Live Demo.py
+++ my_app/pages/3 Algorithm Live Demo.py
@@ -130,8 +130,7 @@ def getAttributesOfTweet(tweetNumber, df):
             stringOfNotEmotion = stringOfNotEmotion + emotionText
             countEmotionsPassive = countEmotionsPassive + 1
     if(countEmotionsActive > 1):
-        st.text('Streamlit is **_really_ cool**.')
-        st.markdown("The algorithm determined the emotions **" + stringOfEmotion + "** in the tweet based on the training.")
+        st.markdown("The algorithm determined <h1>the emotions<h1> **" + stringOfEmotion + "** in the tweet based on the training.")
     elif(countEmotionsActive == 1):
         st.markdown("The algorithm determined the emotion __**" + stringOfEmotion +"**__  in the tweet based on the training.")
     else:
